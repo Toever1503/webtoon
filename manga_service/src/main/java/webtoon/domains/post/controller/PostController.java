511e7b1ccfe53367c1155e289522e8ed3a29f739
Toever1503 <Haunv1503@gmail.com>
2023-03-16 10:07:45 +0700

    feat: add thymeleaf configure

--- manga_service/src/main/java/webtoon/domains/post/controller/PostController.java
+++ manga_service/src/main/java/webtoon/domains/post/controller/PostController.java
@@ -1,5 +1,39 @@
 package webtoon.domains.post.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.ModelAndView;
+
+import webtoon.domains.post.service.IPostService;
+
+@Controller
+@RequestMapping("post")
 public class PostController {
 
+	private final IPostService postService;
+	private final Logger logger = LoggerFactory.getLogger(this.getClass());
+
+	public PostController(IPostService postService) {
+		super();
+		this.postService = postService;
+	}
+
+	@GetMapping
+	public ModelAndView test() {
+		ModelAndView modelAndView = new ModelAndView("layout");
+		return modelAndView;
+	}
+
+	@GetMapping("{postName}/{id}")
+	public String detailPostPage(@PathVariable Long id, Model model) {
+		this.logger.info("Entering detail post page with id: {}", id);
+//		model.addAttribute("postModel", this.postService.findByID(id));
+		return "posts/detail-post";
+	}
 }
--- manga_service/src/main/java/webtoon/domains/post/service/PostServiceImpl.java
+++ manga_service/src/main/java/webtoon/domains/post/service/PostServiceImpl.java
@@ -15,48 +15,54 @@ import java.util.ArrayList;
 import java.util.List;
 
 @Service
-public class PostServiceImpl implements IPostService{
-    private final IPostRepository postRepository;
-    private final ITagRepository tagRepository;
-    private final ITagRelationRepository tagRelationRepository;
-    private final PostMapper postMapper;
+public class PostServiceImpl implements IPostService {
+	private final IPostRepository postRepository;
+	private final ITagRepository tagRepository;
+	private final ITagRelationRepository tagRelationRepository;
+	private final PostMapper postMapper;
 
-    public PostServiceImpl(IPostRepository postRepository, ITagRepository tagRepository, ITagRelationRepository tagRelationRepository, PostMapper postMapper) {
-        this.postRepository = postRepository;
-        this.tagRepository = tagRepository;
-        this.tagRelationRepository = tagRelationRepository;
-        this.postMapper = postMapper;
-    }
+	public PostServiceImpl(IPostRepository postRepository, ITagRepository tagRepository,
+			ITagRelationRepository tagRelationRepository, PostMapper postMapper) {
+		this.postRepository = postRepository;
+		this.tagRepository = tagRepository;
+		this.tagRelationRepository = tagRelationRepository;
+		this.postMapper = postMapper;
+	}
 
-    @Override
-    public PostDto savePost(PostInput input) {
-        PostEntity entity = this.postMapper.toEntity(input);
-        this.postRepository.saveAndFlush(entity);
+	@Override
+	public PostDto savePost(PostInput input) {
+		PostEntity entity = this.postMapper.toEntity(input);
+		this.postRepository.saveAndFlush(entity);
 
-        if (input.getTagRelations() != null) {
-            List<TagEntityRelation> tags = new ArrayList<>();
+		if (input.getTagRelations() != null) {
+			List<TagEntityRelation> tags = new ArrayList<>();
 
-            input.getTagRelations().forEach(t -> {
-                TagEntity tag = this.tagRepository
-                        .findByTagName(t)
-                        .orElse(TagEntity.builder().tagName(t).build());
-                TagEntityRelation relation = this.tagRelationRepository.findByObjectIDAndTagType(entity.getId(), "post");
-                if (relation != null) {
-                    tags.add(relation);
-                }
-                else
-                    tags.add(TagEntityRelation.builder()
-                            .objectID(entity.getId())
-                            .tag(tag)
-                                    .tagType("post")
-                            .build());
-            });
+			input.getTagRelations().forEach(t -> {
+				TagEntity tag = this.tagRepository.findByTagName(t).orElse(TagEntity.builder().tagName(t).build());
+				TagEntityRelation relation = this.tagRelationRepository.findByObjectIDAndTagType(entity.getId(),
+						"post");
+				if (relation != null) {
+					tags.add(relation);
+				} else
+					tags.add(TagEntityRelation.builder().objectID(entity.getId()).tag(tag).tagType("post").build());
+			});
 
-            this.tagRelationRepository.saveAllAndFlush(tags);
-            entity.setTagRelations(tags);
-        }
+			this.tagRelationRepository.saveAllAndFlush(tags);
+			entity.setTagRelations(tags);
+		}
 
-        return this.postMapper.toDto(entity);
-    }
+		return this.postMapper.toDto(entity);
+	}
+
+	@Override
+	public PostDto findByID(Long postID) {
+		// TODO Auto-generated method stub
+		return this.postMapper.toDto(this.getByID(postID));
+	}
+
+	@Override
+	public PostEntity getByID(Long ID) {
+		// TODO Auto-generated method stub
+		return this.postRepository.findById(ID).orElseThrow(() -> new RuntimeException("dasdas"));
+	}
 }
-
