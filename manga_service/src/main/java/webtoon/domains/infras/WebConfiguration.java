170d332d23d16cdbdf638f1c2d0f5fd3745ded08
Toever1503 <Haunv1503@gmail.com>
2023-03-22 03:33:57 +0900

    update

--- manga_service/src/main/java/webtoon/domains/infras/WebConfiguration.java
+++ manga_service/src/main/java/webtoon/domains/infras/WebConfiguration.java
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.event.EventListener;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.thymeleaf.TemplateEngine;
 
@@ -26,5 +27,11 @@ public class WebConfiguration implements WebMvcConfigurer {
 	@Bean
 	public LayoutDialect layoutDialect() {
 		return new LayoutDialect();
+	}
+
+	@Override
+	public void addViewControllers(ViewControllerRegistry registry) {
+		registry.addViewController("/").setViewName("homepage");
+//		WebMvcConfigurer.super.addViewControllers(registry); error
 	}
 }
