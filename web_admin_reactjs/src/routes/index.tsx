2a3c6e577a3ea2b885b5c7782d7d91239426a3cd
CongDuy <rudbich1999@gmail.com>
2023-03-21 15:40:55 +0700

    haha

--- web_admin_reactjs/src/routes/index.tsx
+++ web_admin_reactjs/src/routes/index.tsx
@@ -1,21 +1,31 @@
-import { createBrowserRouter } from "react-router-dom";
+import {createBrowserRouter} from "react-router-dom";
 import DefaultLayout from "../layouts/DefaultLayout";
 import CounterPage from "../pages/counter/CounterPage";
+import MangaPage from "../pages/manga/MangaPage";
+import AddNewManga from "../pages/manga/component/AddNewManga";
 
 const router = createBrowserRouter([
     {
         path: "/",
-        element: <DefaultLayout />,
+        element: <DefaultLayout/>,
         children: [
             {
-              path: "/contacts",
-              element: <CounterPage />,
+                path: "/contacts",
+                element: <CounterPage/>,
             },
-          ],
+            {
+                path: "/manga",
+                element: <MangaPage/>,
+            },
+            {
+                path: "/manga/add",
+                element: <AddNewManga/>
+            },
+        ],
     },
     {
         path: "/counter",
-        element: <CounterPage />
+        element: <CounterPage/>
     }
 ]);
 
