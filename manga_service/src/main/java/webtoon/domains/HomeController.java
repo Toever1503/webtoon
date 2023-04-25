b5799e9763f8d542fc57ed8594c703fda53a1584
Toever1503 <Haunv1503@gmail.com>
2023-04-25 20:27:02 +0700

    fix: conflict

--- manga_service/src/main/java/webtoon/domains/HomeController.java
+++ manga_service/src/main/java/webtoon/domains/HomeController.java
@@ -4,13 +4,16 @@ package webtoon.domains;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.domain.Specification;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import webtoon.account.entities.UserEntity;
 import webtoon.domains.manga.entities.MangaEntity;
+import webtoon.domains.manga.entities.MangaEntity_;
 import webtoon.domains.manga.entities.MangaGenreEntity;
+import webtoon.domains.manga.enums.EStatus;
 import webtoon.domains.post.service.ICategoryService;
 import webtoon.domains.post.service.IPostService;
 import webtoon.domains.manga.services.IMangaChapterService;
@@ -20,6 +23,7 @@ import webtoon.domains.post.entities.PostEntity;
 
 import javax.servlet.http.HttpSession;
 import java.util.List;
+
 @Controller
 public class HomeController {
 
@@ -40,7 +44,11 @@ public class HomeController {
 
     @RequestMapping("/index")
     public String homepage(Model model, Pageable pageable, @RequestParam(required = false, name = "login-type") Integer hasLogged, HttpSession session) {
-        Page<MangaEntity> mangaEntity = this.mangaService.filterEntities(pageable, null);
+        Specification mangaSpec = Specification.where(
+                (root, query, cb) -> cb.equal(root.get(MangaEntity_.STATUS), EStatus.DRAFTED).not()
+        ).and((root, query, cb) -> cb.isNull(root.get(MangaEntity_.DELETED_AT)));
+
+        Page<MangaEntity> mangaEntity = this.mangaService.filterEntities(pageable, mangaSpec);
 
 //        List<CategoryEntity> categoryEntity = this.categoryService.findAllCate();
 
@@ -51,6 +59,7 @@ public class HomeController {
         model.addAttribute("genreList", mangaGenreEntity);
 //        model.addAttribute("cateList", categoryEntity);
         model.addAttribute("mangalist", mangaEntity.getContent());
+        model.addAttribute("mangaSlider", mangaEntity.getContent());
         model.addAttribute("postList", postEntity);
 
         if (hasLogged != null) {
