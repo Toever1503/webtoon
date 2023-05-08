b9c1729b35c48d575c25213f0af5f9ac2348f508
Toever1503 <Haunv1503@gmail.com>
2023-05-08 14:39:23 +0700

    feat: add send mail order info

--- payment_service/src/main/java/webtoon/payment/services/impl/SendEmailServiceImpl.java
+++ payment_service/src/main/java/webtoon/payment/services/impl/SendEmailServiceImpl.java
@@ -1,9 +1,12 @@
 package webtoon.payment.services.impl;
 
 import lombok.RequiredArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
+import org.thymeleaf.TemplateEngine;
 import webtoon.account.configs.security.SecurityUtils;
 import webtoon.account.entities.UserEntity;
 import webtoon.payment.services.ISendEmail;
@@ -11,12 +14,16 @@ import webtoon.payment.services.ISendEmail;
 import javax.mail.MessagingException;
 import javax.mail.internet.MimeMessage;
 import java.nio.charset.StandardCharsets;
+import java.util.Map;
+import org.thymeleaf.context.Context;
 
 @Service
 @RequiredArgsConstructor
 public class SendEmailServiceImpl implements ISendEmail {
 
     private final JavaMailSender javaMailSender;
+    private final TemplateEngine templateEngine;
+    private final Logger mailLogger = LoggerFactory.getLogger(this.getClass());
 
     @Override
     public void sendingPayment(String email) {
@@ -38,5 +45,27 @@ public class SendEmailServiceImpl implements ISendEmail {
         } catch (MessagingException e) {
             e.printStackTrace();
         }
+    }
+
+    @Override
+    public void sendMail(String templatePath, String to, String subject, Map<String, Object> content) throws MessagingException {
+        mailLogger.info("Begin send mail");
+        MimeMessage message = javaMailSender.createMimeMessage(); // init new SimpleMailMessage
+        MimeMessageHelper messageHelper = new MimeMessageHelper(message, true, "UTF-8");
+
+        //add variable
+        Context context = new Context();
+        context.setVariables(content);
+
+        String contentHtml = templateEngine.process(templatePath, context);
+
+        // set recipient, subject, content
+        messageHelper.setFrom("noreply@animenews.life");
+        messageHelper.setTo(to);
+        messageHelper.setSubject(subject);
+        messageHelper.setText(contentHtml, true);
+
+        javaMailSender.send(message); // send mail
+        mailLogger.info("Send mail to ".concat(to.concat(" successfully!")));
     }
 }
