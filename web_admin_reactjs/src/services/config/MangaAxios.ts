dc8cfcd7a8007c2c4cdba16456cf75899f8220c1
Toever1503 <Haunv1503@gmail.com>
2023-05-16 15:47:30 +0700

    update: content

--- web_admin_reactjs/src/services/config/MangaAxios.ts
+++ web_admin_reactjs/src/services/config/MangaAxios.ts
@@ -7,7 +7,7 @@ function getInstance(): AxiosInstance {
         return axiosInstance
     }
     axiosInstance = axios.create({
-        baseURL: "http://localhost:8080/api",
+        baseURL: "http://34.81.192.98:8080/api",
         headers: {},
     });
 
--- web_admin_reactjs/src/services/user/UserService.ts
+++ web_admin_reactjs/src/services/user/UserService.ts
@@ -1,22 +1,22 @@
 import { IUserStatus } from "../../pages/user/types/IUserType";
-import { userAxios } from "../config/UserAxios";
+import { mangaAxios} from "../config/MangaAxios";
 import IUserFilterInputType from "./types/IUserFilterInputType";
 import IUserInputType from "./types/IUserInputType";
 
 
 const basePath: String = "/users";
 
-const findUserById = async (id: number | string) => userAxios.get(`${basePath}/${id}`);
-const addNewUser = async (payload: IUserInputType) => userAxios.post(`${basePath}`, payload);
-const updateUser = async (id: number | string, payload: IUserInputType) => userAxios.put(`${basePath}/${id}`, payload);
-const deleteUserById = async (id: number | string) => userAxios.del(`${basePath}/${id}`);
-const filterUser = async (payload: IUserFilterInputType, page: number, size: number) => userAxios.post(`${basePath}/filter?page=${page}&size=${size}&sort=id,desc`, payload);
-const changeStatus = (id: number | string, status: IUserStatus) => userAxios.patch(`${basePath}/${id}/change-status?status=${status}`);
+const findUserById = async (id: number | string) => mangaAxios.get(`${basePath}/${id}`);
+const addNewUser = async (payload: IUserInputType) => mangaAxios.post(`${basePath}`, payload);
+const updateUser = async (id: number | string, payload: IUserInputType) => mangaAxios.put(`${basePath}/${id}`, payload);
+const deleteUserById = async (id: number | string) => mangaAxios.del(`${basePath}/${id}`);
+const filterUser = async (payload: IUserFilterInputType, page: number, size: number) => mangaAxios.post(`${basePath}/filter?page=${page}&size=${size}&sort=id,desc`, payload);
+const changeStatus = (id: number | string, status: IUserStatus) => mangaAxios.patch(`${basePath}/${id}/change-status?status=${status}`);
 
-const getAllAuthorities = async () => userAxios.get(`${basePath}/all-authorities`);
+const getAllAuthorities = async () => mangaAxios.get(`${basePath}/all-authorities`);
 
-const forgotPassword = async (email: string) => userAxios.post(`${basePath}/forgot-password?email=${email}`);
-const signin = async (input: any) => userAxios.post(`${basePath}/signin`, input);
+const forgotPassword = async (email: string) => mangaAxios.post(`${basePath}/forgot-password?email=${email}`);
+const signin = async (input: any) => mangaAxios.post(`${basePath}/signin`, input);
 
 const userService = {
     findUserById,
