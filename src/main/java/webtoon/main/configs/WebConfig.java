57132c1377d5e64f261cbbaa5b7fb355f6f823ce
Toever1503 <Haunv1503@gmail.com>
2023-05-21 16:32:38 +0700

    Update WebConfig.java

--- src/main/java/webtoon/main/configs/WebConfig.java
+++ src/main/java/webtoon/main/configs/WebConfig.java
@@ -98,7 +98,8 @@ public class WebConfig implements WebMvcConfigurer {
                                 "http://localhost:5173",
                                 "http://127.0.0.1:5173",
                                 "http://35.229.243.12:5173",
-                                "https://shikishiki.site"
+                                "https://shikishiki.site",
+                                "34.81.192.98:8080"
                         )
                         .allowedOriginPatterns("*.*.*.*:*")
                         .allowCredentials(true)
