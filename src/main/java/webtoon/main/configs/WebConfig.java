bb193e7bef40442cf3a9305ba29d14d53661525d
Toever1503 <Haunv1503@gmail.com>
2023-05-21 16:35:20 +0700

    add cors

--- src/main/java/webtoon/main/configs/WebConfig.java
+++ src/main/java/webtoon/main/configs/WebConfig.java
@@ -98,7 +98,9 @@ public class WebConfig implements WebMvcConfigurer {
                                 "http://localhost:5173",
                                 "http://127.0.0.1:5173",
                                 "http://35.229.243.12:5173",
-                                "https://shikishiki.site",
+                                "shikishiki.site",
+                                "34.81.192.98:80",
+                                "34.81.192.98:443",
                                 "34.81.192.98:8080"
                         )
                         .allowedOriginPatterns("*.*.*.*:*")
