6bab1cdf0afb9573fc7bda06fd3e8cfb0780dfbb
Toever1503 <Haunv1503@gmail.com>
2023-05-22 17:02:17 +0700

    fix get manga view

--- src/main/java/webtoon/main/domains/manga/repositories/IMangaViewCountEntityRepository.java
+++ src/main/java/webtoon/main/domains/manga/repositories/IMangaViewCountEntityRepository.java
@@ -3,12 +3,14 @@ package webtoon.main.domains.manga.repositories;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import webtoon.main.domains.manga.entities.MangaViewCountEntity;
 
 public interface IMangaViewCountEntityRepository extends JpaRepository<MangaViewCountEntity, Long>, JpaSpecificationExecutor<MangaViewCountEntity> {
     @Query("select sum(mvc.count) from MangaViewCountEntity mvc where mvc.manga.id = ?1")
     Long countViewByMangaId(Long mangaId);
 
-    @Query("select mvc from MangaViewCountEntity mvc where mvc.manga.id = ?1 and mvc.createdAt = current_date")
-    MangaViewCountEntity findViewCountTodayForMangaId(Long mangaId);
+    @Query(value = "SELECT * FROM tbl_manga_view_count\n" +
+            "where manga_id = :mangaId and created_at = current_date()", nativeQuery = true)
+    MangaViewCountEntity findViewCountTodayForMangaId(@Param("mangaId") Long mangaId);
 }
